2284a54c31b59bb116179841d0c5c3cf01cc0547
M-imb <70661492+M-imb@users.noreply.github.com>
2025-07-24 23:27:44 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,7 @@ from sklearn.neighbors import KNeighborsClassifier
 import category_encoders as ce
 import plotly.express as px
 
+
 st.set_page_config(page_title="🐧 Penguin Classifier", layout="wide")
 st.title('🐧 Penguin Classifier - Обучение и предсказание')
 st.write('## Работа с датасетом пингвинов')
