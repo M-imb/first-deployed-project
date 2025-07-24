1872a0a52ae6a5315781e2288675dcbd4916bbdb
M-imb <70661492+M-imb@users.noreply.github.com>
2025-07-25 03:18:36 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,3 +71,16 @@ bill_length = st.sidebar.slider("Длина клюва (мм)", float(df['bill_l
 bill_depth = st.sidebar.slider("Глубина клюва (мм)", float(df['bill_depth_mm'].min()), float(df['bill_depth_mm'].max()), float(df['bill_depth_mm'].mean()))
 flipper_length = st.sidebar.slider("Длина крыла (мм)", float(df['flipper_length_mm'].min()), float(df['flipper_length_mm'].max()), float(df['flipper_length_mm'].mean()))
 body_mass = st.sidebar.slider("Масса тела (г)", float(df['body_mass_g'].min()), float(df['body_mass_g'].max()), float(df['body_mass_g'].mean()))
+
+user_input = pd.DataFrame([[
+    'island' : island_input,
+    'sex' : sex_input,
+    'bill_length_mm': bill_length,
+    'bill_depth_mm': bill_depth,
+    'flipper_length_mm': flipper_length,
+    'body_mass_g': body_mass
+]])
+user_encoded = encoder.transform(user_input)
+for col in ['bill_length_mm', 'bill_depth_mm', 'flipper_length_mm', 'body_mass_g']:
+    user_encoded[col] = user_input[col].values
+user_encoded = user_encoded[X_train_encoded.columns]
